b96d2213aa819fd8e6ef24341f6e951bd2bbca1f
Adeline <adeline.sire@gmail.com>
2022-06-01 12:24:55 +0200

    Sets up backend

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(express.static('public'));
+
+app.listen(process.env.PORT || 3000, () => console.log('Server started'));
